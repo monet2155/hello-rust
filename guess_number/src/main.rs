da25e90677a72d560c24ac71de8b3c9887ea842d
monet2155 <monet9812@gmail.com>
2024-07-18 13:55:17 +0900

    feat: number match game with comments

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,6 +1,9 @@
+// you can use std::cmp::Ordering, std::io
+// or like this
+use std::{cmp::Ordering, io};
+// rand is crate, which is collection of rust code files
+// Rng is trait
 use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -10,14 +13,36 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
+        // let make variables
+        // let apples = 5; is also make variable, but immutable(불변)
+        // In rust, variable is immutable basically
         let mut guess = String::new();
 
         io::stdin()
+            // & is reference. And reference type is immutable
+            // So require to change reference mutable
             .read_line(&mut guess)
+            // read_line() returns Result type.
+            // Result type is enumeration
+            // Result can get Ok or Err, which called variant
             .expect("Failed to read line");
 
+        if guess.trim().eq("quit") {
+            break;
+        }
+
+        // match guess.cmp(&secret_number) {
+        // This line can't compiled.
+        // guess is String, but secret_number is integer type(i32)
+        // So add this line
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // rust can declare duplicated name variable, which called shdowing
+        // shadowing usually used when want to change type of variable
+
+        // If user input not integer value, continue loop
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
+            // _ is called catch all value
             Err(_) => continue,
         };
 
@@ -27,7 +52,7 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("you win!");
                 break;
             }
         }
