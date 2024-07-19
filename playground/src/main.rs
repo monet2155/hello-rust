aa0155544c42e4ebe78c0cd01984583cd4849e6b
monet2155 <monet9812@gmail.com>
2024-07-19 16:20:19 +0900

    play: some complie errors

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,7 +3,17 @@ fn main() {
 
     println!("{}", x);
 
-    x.push_str(", TEST2");
+    // x.push_str(", TEST2");
 
-    println!("{}", x);
+    // println!("{}", x);
+
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+
+    // println!("{}, world!", s1);
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
